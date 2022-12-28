115f0828b8cbc564da7e3d0fbf41ff53999de572
priyagupta2893 <priya.priyagupta93@gmail.com>
2022-12-28 17:16:23 +0530

    my project

---
+++ src/test/java/com/cdp/dependencyInjectionProject/DependencyInjectionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdp.dependencyInjectionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
